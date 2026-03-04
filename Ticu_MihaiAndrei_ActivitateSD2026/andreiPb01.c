ee89614723a4d6a8d1ee59e67a93c27f53184cb1
xzAndreixz <ticumihaiandrei27@gmail.com>
2026-03-04 12:57:28 +0200

    Citirea unui numar intreg de la tastatura

--- Ticu_MihaiAndrei_ActivitateSD2026/andreiPb01.c
+++ Ticu_MihaiAndrei_ActivitateSD2026/andreiPb01.c
@@ -5,5 +5,11 @@ int main() {
 
 	printf("Salut, Andrei!\n");
 
+	int var = 0;
+	printf("Te rog sa introduci un numar intreg:");
+	scanf_s("%d", &var);
+
+	printf("Ai introdus: %d", var);
+
 	return 0;
 }
