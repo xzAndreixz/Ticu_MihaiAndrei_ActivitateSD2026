9bbb1199ecef3950cba60b6d5c9bcb00e22b266c
xzAndreixz <ticumihaiandrei27@gmail.com>
2026-03-04 15:17:15 +0200

    Functia de intializare

--- Ticu_MihaiAndrei_ActivitateSD2026/seminar01.cpp
+++ Ticu_MihaiAndrei_ActivitateSD2026/seminar01.cpp
@@ -1,17 +1,32 @@
 #include <stdio.h>
 #include <malloc.h>
+#include <string.h>
+
+typedef struct Depozit Depozit;
+
+struct Depozit {
+	int id;
+	float suprafata;
+	int volum;
+	char* denumire;
+	char clasa;
+};
+
+struct Depozit intializare(int id, float suprafata, int volum, const char* denumire, char clasa) {
+	struct Depozit s;
+	s.id = id;
+	s.suprafata = suprafata;
+	s.volum = volum;
+	s.denumire = (char*)malloc(sizeof(char) * (strlen(denumire) + 1));
+	strcpy(s.denumire, denumire);
+	return s;
+}
 
 int main() {
 
-	typedef struct Depozit Depozit;
+	
 
-	struct Depozit {
-		int id;
-		float suprafata;
-		int volum;
-		char* denumire;
-		char clasa;
-	};
+
 
 
 	return 0;
