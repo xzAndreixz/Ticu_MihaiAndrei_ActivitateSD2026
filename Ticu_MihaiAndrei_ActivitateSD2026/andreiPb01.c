0424151b2b147002acf61b29a5fd63c47a3d12cc
xzAndreixz <ticumihaiandrei27@gmail.com>
2026-03-04 13:03:58 +0200

    Citirea si afisarea unei variabile reale

--- Ticu_MihaiAndrei_ActivitateSD2026/andreiPb01.c
+++ Ticu_MihaiAndrei_ActivitateSD2026/andreiPb01.c
@@ -9,7 +9,12 @@ int main() {
 	printf("Te rog sa introduci un numar intreg:");
 	scanf_s("%d", &var);
 
-	printf("Ai introdus: %d", var);
+	printf("Ai introdus: %d \n", var);
+
+	float s;
+	printf("Introdu o variabila reala: ");
+	scanf_s("%f", &s);
+	printf("Ai introdus: %f", s);
 
 	return 0;
 }
