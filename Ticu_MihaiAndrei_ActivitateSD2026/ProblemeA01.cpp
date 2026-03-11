98c48dcb62545ad3b11509a319251798c212ab9e
xzAndreixz <ticumihaiandrei27@gmail.com>
2026-03-11 14:12:32 +0200

    Declarare structura

---
+++ Ticu_MihaiAndrei_ActivitateSD2026/ProblemeA01.cpp
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include<string>
+struct Rezervare {
+
+	int id;
+	char numeClient[50];
+	int numarCamera;
+	int nopti;
+	float pretPerNoapte;
+};
+
+
+
+
+int main() {
+
+
+
+
+	return 0;
+}
