8e70498e33263a078b33732fbddb57d61f6c1695
xzAndreixz <ticumihaiandrei27@gmail.com>
2026-03-11 14:13:51 +0200

    functia de initializare a structurii

--- Ticu_MihaiAndrei_ActivitateSD2026/ProblemeA01.cpp
+++ Ticu_MihaiAndrei_ActivitateSD2026/ProblemeA01.cpp
@@ -10,12 +10,26 @@ struct Rezervare {
 	float pretPerNoapte;
 };
 
+struct Rezervare initializare(int id, const char numeClient[50], int numarCamera, int nopti, float pretPerNoapte) {
+	struct Rezervare i1;
+	i1.id = id;
+	if (strlen(numeClient) < 50)
+		strcpy(i1.numeClient, numeClient);
+	else {
+		printf("Ai depasit numarul de caractere!");
+	}
+	i1.numarCamera = numarCamera;
+	i1.nopti = nopti;
+	i1.pretPerNoapte = pretPerNoapte;
+
+	return i1;
+}
 
 
 
 int main() {
 
-
+	
 
 
 	return 0;
--- Ticu_MihaiAndrei_ActivitateSD2026/seminar01.cpp
+++ Ticu_MihaiAndrei_ActivitateSD2026/seminar01.cpp
@@ -1,33 +1,34 @@
-#include <stdio.h>
-#include <malloc.h>
-#include <string.h>
-
-typedef struct Depozit Depozit;
-
-struct Depozit {
-	int id;
-	float suprafata;
-	int volum;
-	char* denumire;
-	char clasa;
-};
-
-struct Depozit intializare(int id, float suprafata, int volum, const char* denumire, char clasa) {
-	struct Depozit s;
-	s.id = id;
-	s.suprafata = suprafata;
-	s.volum = volum;
-	s.denumire = (char*)malloc(sizeof(char) * (strlen(denumire) + 1));
-	strcpy(s.denumire, denumire);
-	return s;
-}
-
-int main() {
-
-	
-
-
-
-
-	return 0;
-}
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//#include <malloc.h>
+//#include <string.h>
+//
+//typedef struct Depozit Depozit;
+//
+//struct Depozit {
+//	int id;
+//	float suprafata;
+//	int volum;
+//	char* denumire;
+//	char clasa;
+//};
+//
+//struct Depozit intializare(int id, float suprafata, int volum, const char* denumire, char clasa) {
+//	struct Depozit s;
+//	s.id = id;
+//	s.suprafata = suprafata;
+//	s.volum = volum;
+//	s.denumire = (char*)malloc(sizeof(char) * (strlen(denumire) + 1));
+//	strcpy(s.denumire, denumire);
+//	return s;
+//}
+//
+//int main() {
+//
+//	Depozit dep1 = intializare(1, 100, 300, "DEP1", 'A');
+//
+//	printf("%d", dep1.id);
+//
+//
+//	return 0;
+//}
