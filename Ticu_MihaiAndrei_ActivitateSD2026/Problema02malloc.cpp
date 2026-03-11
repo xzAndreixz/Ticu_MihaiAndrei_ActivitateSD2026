3a0e6dc8936f98c7c3f6a0eaa93f22d85440af98
xzAndreixz <ticumihaiandrei27@gmail.com>
2026-03-11 15:56:52 +0200

    functia de initializare

--- Ticu_MihaiAndrei_ActivitateSD2026/Problema02malloc.cpp
+++ Ticu_MihaiAndrei_ActivitateSD2026/Problema02malloc.cpp
@@ -1,8 +1,28 @@
 #include <stdio.h>
+#include<string.h>
+#include<stdlib.h>
 
+struct Film {
+	int id;
+	char* titlu;
+	float rating;
+	int anAparitie;
+};
+
+struct Film initializare(int id, const char* titlu, float rating, int anAparitie) {
+	struct Film f;
+	f.id = id;
+	f.titlu = (char*)malloc(sizeof(char) * (strlen(titlu) + 1));
+	strcpy_s(f.titlu, strlen(titlu) + 1, titlu);
+	f.rating = rating;
+	f.anAparitie = anAparitie;
+
+	return f;
+}
 
 int main() {
 
+	
 
 	return 0;
 }
