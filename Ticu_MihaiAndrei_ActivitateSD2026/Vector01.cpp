09fca43100963458a2d4b5fb3a4f3f71868491fd
xzAndreixz <ticumihaiandrei27@gmail.com>
2026-03-13 14:03:38 +0200

    Functiile de initializare cu si fara parametri

--- Ticu_MihaiAndrei_ActivitateSD2026/Vector01.cpp
+++ Ticu_MihaiAndrei_ActivitateSD2026/Vector01.cpp
@@ -2,11 +2,34 @@
 #include <stdlib.h>
 #include <string.h>
 
+struct Caiet {
+	char* tip;
+	int nrPg;
+};
+
+struct Caiet initializare() {
+	struct Caiet c;
+	c.nrPg = 60;
+	c.tip = (char*)malloc(sizeof(char) * strlen("Matematica") + 1);
+	strcpy_s(c.tip, strlen("Matematica") + 1, "Matematica");
+
+	return c;
+}
+
+
+struct Caiet initializare(const char* tip, int nrPg) {
+	struct Caiet c;
+	c.nrPg = nrPg;
+	c.tip = (char*)malloc(sizeof(char) * strlen(tip) + 1);
+	strcpy_s(c.tip, strlen(tip) + 1, tip);
+
+	return c;
+}
 
 
 int main() {
 
-	printf("Andrei");
+	
 
 	return 0;
 }
