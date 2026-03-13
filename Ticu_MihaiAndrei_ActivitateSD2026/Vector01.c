5265084a3a5f8ee797528a158b375f76c0815b61
xzAndreixz <ticumihaiandrei27@gmail.com>
2026-03-13 15:20:44 +0200

    Am realizat ca le-am salvat cu extensia .cpp si am modificat asta :))

---
+++ Ticu_MihaiAndrei_ActivitateSD2026/Vector01.c
@@ -0,0 +1,99 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct Caiet {
+	char* tip;
+	int nrPg;
+};
+
+struct Caiet initializare() {
+	struct Caiet c;
+	c.nrPg = 60;
+	c.tip = (char*)malloc(sizeof(char) * strlen("Matematica") + 1);
+	strcpy_s(c.tip, strlen("Matematica") + 1, "Matematica");
+
+	return c;
+}
+
+
+struct Caiet initializare(const char* tip, int nrPg) {
+	struct Caiet c;
+	c.nrPg = nrPg;
+	c.tip = (char*)malloc(sizeof(char) * strlen(tip) + 1);
+	strcpy_s(c.tip, strlen(tip) + 1, tip);
+
+	return c;
+}
+
+void afisare(struct Caiet c) {
+	printf("Tip: %s\n", c.tip);
+	printf("Nr. pagini: %d\n\n", c.nrPg);
+}
+
+void afisareVector(struct Caiet* a, int dim) {
+	for (int i = 0; i < dim; i++) {
+		afisare(a[i]);
+	}
+}
+
+struct Caiet* copaizaPrimeleNElemente(struct Caiet* c, int dim, int dimCopiaza) {
+	if (dimCopiaza > dim) { printf("Eroare, nu poti copia mai mult de %d elemente", dim); return 0; }
+	struct Caiet* v = (struct Caiet*)malloc(sizeof(struct Caiet) * dimCopiaza);
+	for (int i = 0; i < dimCopiaza; i++) {
+		v[i] = c[i];
+		v[i].tip = (char*)malloc(sizeof(char) * (strlen(c[i].tip) + 1));
+	}
+
+	return v;
+}
+
+void dezalocare(struct Caiet** v, int* nrElemente) {
+	for (int i = 0; i < (*nrElemente); i++) {
+		if ((*v)[i].tip != NULL)
+			free((*v)[i].tip);
+	}
+	free((*v));
+	(*v) = NULL;
+	(*nrElemente) = 0; 
+}
+
+struct Caiet* copaizaCaiete(struct Caiet* c, int dim, int nrPg,int* dimVcopiat){ // caietele care au mai mult de 80 de pagini
+	struct Caiet* vector;
+	int mem = 0;
+	for (int i = 0; i < dim; i++) {
+		if (c[i].nrPg > 80) mem++;
+	}
+	vector = (struct Caiet*)malloc(sizeof(struct Caiet) * mem);
+	int j = 0;
+	for (int i = 0; i < dim; i++) {
+		if (c[i].nrPg > 80) {
+			vector[j] = c[i];
+			vector[j].tip = (char*)malloc(sizeof(char) * (strlen(c[i].tip) + 1));
+			strcpy_s(vector[j].tip, strlen(c[i].tip) + 1, c[i].tip);
+			j++;
+		
+		}
+	}
+	(*dimVcopiat) = mem;
+	return vector;
+}
+int main() {
+	int nrCaiete = 3;
+	struct Caiet* caieteDiv = (struct Caiet*)malloc(sizeof(struct Caiet) * nrCaiete);
+
+	caieteDiv[0] = initializare("Mate", 150);
+	caieteDiv[1] = initializare("Rom", 100);
+	caieteDiv[2] = initializare();
+
+
+	int dimNrCaiete;
+	struct Caiet* caietePeste80File;
+	caietePeste80File = copaizaCaiete(caieteDiv, nrCaiete, 80, &dimNrCaiete);
+
+	
+
+
+	
+	return 0;
+}
