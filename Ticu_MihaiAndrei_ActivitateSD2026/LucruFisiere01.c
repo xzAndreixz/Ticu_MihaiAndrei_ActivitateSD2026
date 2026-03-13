3fb573915377a9a6a04f5f1c2c55704494d32771
xzAndreixz <ticumihaiandrei27@gmail.com>
2026-03-13 22:51:31 +0200

    Functia afisare Masina si Vector de masini

--- Ticu_MihaiAndrei_ActivitateSD2026/LucruFisiere01.c
+++ Ticu_MihaiAndrei_ActivitateSD2026/LucruFisiere01.c
@@ -2,6 +2,34 @@
 #include<stdlib.h>
 #include<string.h>
 
+typedef struct {
+	int id;
+	int nrUsi;
+	float pret;
+	char* model;
+	char* numeSofer;
+	unsigned char serie;
+}Masina;
+
+void afisareMasina(Masina m) {
+	printf("Id: %d\n", m.id);
+	printf("Nr. usi: %d\n", m.nrUsi);
+	printf("Pret %.2f\n", m.pret);
+	printf("Model: %s\n", m.model);
+	printf("Sofer: %s\n", m.numeSofer);
+	printf("Serie: %c\n", m.serie);
+}
+
+void afisareVectorMasini(Masina* m, int dim) {
+	for (int i = 0; i < dim; i++) {
+		afisareMasina(m[i]);
+	}
+}
+
+
+
+
+
 int main() {
 
 	
