0e2ae438f01fabe38a1bead373faa64baf035ac9
xzAndreixz <ticumihaiandrei27@gmail.com>
2026-03-24 17:40:27 +0200

    Exersare lucru cu lista simplu inlantuita + functie inserareLista + afisareLista

---
+++ Ticu_MihaiAndrei_ActivitateSD2026/listeLucru01.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+typedef struct Nod {
+	int nr;
+	struct Nod* urm;
+}Nod;
+
+void inserareLista(Nod** prim, int x) {
+	Nod* q = (Nod*)malloc(sizeof(Nod));
+	q->nr = x;
+	q->urm = NULL;
+	if ((*prim) == NULL) {
+		(*prim) = q;
+	}
+	else {
+		Nod* pas = (*prim);
+		while (pas->urm != NULL) {
+			pas = pas->urm;
+		}
+		pas->urm = q;
+	}
+}
+void afisareLista(Nod* p) {
+	if (p == NULL) {
+		printf("Lista este goala!\n");
+	}
+	else {
+		Nod* pas = p;
+		while (pas != NULL) {
+			printf("Valoare: %d\n", pas->nr);
+			pas = pas->urm;
+		}
+	}
+}
+int n = 5, i, a;
+int main() {
+
+
+	Nod* p = NULL;
+	for (i = 0; i < n; i++) {
+		printf("Intorduceti in lista: ");
+		scanf_s("%d", &a);
+		inserareLista(&p, a);
+	}
+
+	afisareLista(p);
+
+	return 0;
+}
