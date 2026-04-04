31fe3b3459c1c50ce285662c2fd2520d704b83cb
xzAndreixz <ticumihaiandrei27@gmail.com>
2026-04-04 17:27:21 +0300

    Declarat structura heap + functia de intializare + apelare functia initializare in main

--- Ticu_MihaiAndrei_ActivitateSD2026/problemeheap01.c
+++ Ticu_MihaiAndrei_ActivitateSD2026/problemeheap01.c
@@ -2,9 +2,34 @@
 #include <stdlib.h>
 #include <string.h>
 
+
+typedef struct Masina {
+	int cP;
+	int nrLocuri;
+	float consum;
+}Masina;
+
+Masina initializare(int cP, int nrLocuri, float consum) {
+	Masina m;
+	m.cP = cP;
+	m.nrLocuri = nrLocuri;
+	m.consum = consum;
+	return m;
+}
+
+void afisareMasina(Masina m) {
+	printf("Cai putere: %d\n", m.cP);
+	printf("Nr. locuri: %d\n", m.nrLocuri);
+	printf("Consum: %.2f\n", m.consum);
+}
+
+
+
 int main() {
 
-	printf("Salut, Andrei!");
+	Masina m;
+	m = initializare(110, 5, 4.9);
+	afisareMasina(m);
 
 
 	return 0;
