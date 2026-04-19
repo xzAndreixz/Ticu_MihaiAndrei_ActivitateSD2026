40f9c51e5d4efd59664d0b2ee77c29742c9e1689
xzAndreixz <ticumihaiandrei27@gmail.com>
2026-04-19 12:27:45 +0300

    structura masina

---
+++ Ticu_MihaiAndrei_ActivitateSD2026/heapPb02.c
@@ -0,0 +1,24 @@
+
+#include <stdio.h>
+#include<stdlib.h>
+
+typedef struct Masina {
+	int id;
+	int nrUsi;
+	float pret;
+	char* model;
+	char* numeSofer;
+	unsigned char serie;
+}Masina;
+
+
+
+
+int main() {
+
+
+
+
+	return 0;
+
+}
