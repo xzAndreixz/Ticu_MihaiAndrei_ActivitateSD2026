c52c0a98534cd23109719b48a217b0d0fe8024a0
xzAndreixz <ticumihaiandrei27@gmail.com>
2026-05-18 20:21:37 +0300

    structura masina

---
+++ Ticu_MihaiAndrei_ActivitateSD2026/lucruCuFisiere00.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+typedef struct Masina {
+	int id;
+	int nrUsi;
+	float pret;
+	char* model;
+	char* numeSofer;
+	unsigned char serie;
+}Masina;
+
+
+int main() {
+
+
+
+	return 0;
+}
