4550c79b6030d7ad09daa23c1bd9430269f471c8
xzAndreixz <ticumihaiandrei27@gmail.com>
2026-05-19 15:11:06 +0300

    functia de calcula Hash

--- Ticu_MihaiAndrei_ActivitateSD2026/hashTableLucru02.c
+++ Ticu_MihaiAndrei_ActivitateSD2026/hashTableLucru02.c
@@ -9,6 +9,23 @@ typedef struct Nod {
 	struct Nod* next;
 }Nod;
 
+void insereazaLaSfarsit(Nod** cap, int val) {
+	Nod* nou = (Nod*)malloc(sizeof(Nod));
+	nou->valoare = val;
+	nou->next = NULL;
+
+	if ((*cap) == NULL) (*cap) = nou;
+	else {
+		Nod* curent = (*cap);
+		while (curent->next != NULL) {
+			curent = curent->next;
+		}
+		curent->next = nou;
+	}
+
+}
+
+
 typedef struct HashTable {
 	int dim;
 	Nod** liste;
@@ -23,10 +40,47 @@ HashTable initializare(int dim) {
 	return h;
 }
 
+int calculeazaHash(int val, int dim) {
+	return val % dim;
+}
+
+
+void inserareHash(HashTable h, int val) {
+	int index = calculeazaHash(val, h.dim);
+	insereazaLaSfarsit(&h.liste[index], val);
+
+}
+
+
+void afisareHash(HashTable h) {
+	for (int i = 0; i < h.dim; i++) {
+		if (h.liste[i] != NULL) {
+			Nod* curent = h.liste[i];
+			printf("Index: %d -> ", i);
+			while (curent != NULL) {
+				printf("%d   ", curent->valoare);
+				curent = curent->next;
+			}printf("\n");
+		}
+		else {
+			printf("Index: %d -> NULL\n", i);
+		}
+	}
+}
+
+
 int main() {
 
 	HashTable h1;
 	h1 = initializare(10);
+	inserareHash(h1, 25);
+	inserareHash(h1, 35);
+	inserareHash(h1, 45);
+	inserareHash(h1, 28);
+	inserareHash(h1, 38);
+	inserareHash(h1, 41);
+
+	afisareHash(h1);
 
 
 	return 0;
