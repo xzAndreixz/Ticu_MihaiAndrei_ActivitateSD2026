299f0041ca62907ca88658694285f65568f365ba
xzAndreixz <ticumihaiandrei27@gmail.com>
2026-05-20 09:41:42 +0300

    functia cauta testata in main + corectii ale functiilor anterioare

--- Ticu_MihaiAndrei_ActivitateSD2026/ABC00.c
+++ Ticu_MihaiAndrei_ActivitateSD2026/ABC00.c
@@ -35,6 +35,8 @@ Nod* creeazaNod(Masina m) {
 	nou->info = m;
 	nou->info.model = (char*)malloc(strlen(m.model) + 1);
 	strcpy(nou->info.model, m.model);
+	nou->stanga = NULL;
+	nou->dreapta = NULL;
 
 	return nou;
 }
@@ -47,18 +49,39 @@ Nod* insereaza(Nod* radacina, Masina m) {
 		radacina->stanga = insereaza(radacina->stanga, m);
 	}
 	else if (m.id > radacina->info.id) {
-		radacina->stanga = insereaza(radacina->stanga, m);
+		radacina->dreapta = insereaza(radacina->dreapta, m);
 	}
 
 	return radacina;
 
 }
 
+Nod* cauta(Nod* r, int id) {
+	if (r == NULL || r->info.id == id)
+		return r;
+	if (id < r->info.id)
+		return cauta(r->stanga, id);
+	return cauta(r->dreapta, id);
+}
+
 int main() {
 	Masina m = initializare(1, 1000.5, "Astra");
-	Nod* radacina = NULL;
-	insereaza(radacina, m);
+	Masina m2 = initializare(5, 2000, "Audi");
+	Masina m3 = initializare(15, 3000, "Opel");
 
+	Nod* radacina = NULL;
+	radacina = insereaza(radacina, m);
+	radacina = insereaza(radacina, m2);
+	radacina = insereaza(radacina, m3);
+
+	Nod* rezultat = cauta(radacina, 5);
+
+	if (rezultat != NULL) {
+		printf("Gasit: %s\n", rezultat->info.model);
+	}
+	else {
+		printf("Nu exista!\n");
+	}
 
 
 	return 0;
